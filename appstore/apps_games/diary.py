33c9a58af2047da92ac4256012b961c865697d77
Programmer-Timmy <tim.vanderkloet@gmail.com>
2024-09-10 14:56:30 +0200

    more work for diary

--- appstore/apps_games/diary.py
+++ appstore/apps_games/diary.py
@@ -18,6 +18,10 @@ class Diary:
 
     def __init__(self):
         self.login()
+        if self.logged_in:
+            self.show_diary_entries()
+            entry = self.ask_to_chose_entry()
+            print(entry.title)
 
     def login(self):
         """Log the user in."""
@@ -27,7 +31,6 @@ class Diary:
             print("Je bent ingelogd.")
             self.logged_in = True
             self.get_entries()
-            print(self.entries)
         else:
             print("Ongeldige gebruikersnaam of wachtwoord")
             self.login()
@@ -66,7 +69,59 @@ class Diary:
 
         return self.hash_password(password) == password_hash
 
-    def show_entries(self):
+    def show_diary_entries(self):
+        i = 1
+        print("Opgeslagen dagen:")
+        for entry in self.entries:
+            print(f"{i}: {entry.date}")
+            i += 1
+
+    def ask_to_chose_entry(self) -> Entry:
+        entry_number = input("Welke dag wil je bekijken? ")
+        try:
+            return self.entries[int(entry_number) - 1]
+        except ValueError:
+            print("Ongeldige keuze")
+            return self.ask_to_chose_entry()
+
+    def ask_wat_todo(self):
+        print("Wat wil je doen?")
+        print("1: Nieuwe dag toevoegen")
+        print("2: Dag verwijderen")
+        print("3: Dag aanpassen")
+        print("4: Dag bekijken")
+        print("5: Stoppen")
+        choice = input("Keuze: ")
+
+        match choice:
+            case "1":
+                self.add_new_day()
+            case "2":
+                self.delete_day()
+            case "3":
+                self.edit_day()
+            case "4":
+                self.view_day()
+            case "5":
+                self.stop()
+            case _:
+                print("Ongeldige keuze")
+                self.ask_wat_todo()
+
+    def add_new_day(self):
+        date = input("Wat is de datum van de dag? (dd-mm-jjjj) ")
+        try :
+            date = datetime.strptime(date, "%d-%m-%Y").date()
+        except ValueError:
+            print("Ongeldige datum")
+            self.add_new_day()
+
+        title = input("Wat is de titel van de dag? ")
+        content = input("Wat is de inhoud van de dag? ")
+
+        self.entries.append(Entry(date=date, title=title, content=content))
+
+
 
 
 
